6964d1b427bc973dec01e5dee879c99b854e0932
Tracie Weitzman <tlweitzman@gmail.com>
2017-01-19 17:33:38 -0800

    add app files and bootstrap

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+// import { Meal } from './meal.model';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <h1>Meal Tracker</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
