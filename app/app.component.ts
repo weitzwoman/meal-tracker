065a90327ec979b2041f93988f45f1c5077c3141
Tracie Weitzman <tlweitzman@gmail.com>
2017-01-19 18:02:28 -0800

    add basic methods to app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
-// import { Meal } from './meal.model';
+import { Meal } from './meal.model';
 
 @Component({
   selector: 'app-root',
   template: `
   <div class="container">
     <h1>Meal Tracker</h1>
+    <new-meal></new-meal>
+    <meal-list></meal-list>
+    <edit-meal></edit-meal>
   </div>
   `
 })
 
 export class AppComponent {
+  selectedMeal = null;
 
+  masterMealList: Meal[] =[
+    new Meal('Oatmeal', 'with Blueberries', 240),
+    new Meal('Salad', 'Spinach and Strawberries, and Balsamic Vinegar', 400),
+    new Meal('Pizza', 'three slices from Dove Vivi', 800)
+  ];
+
+  editMeal(clickedMeal) {
+    this.selectedMeal = clickedMeal;
+  }
+
+  finishedEditing() {
+    this.selectedMeal = null;
+  }
+
+  addMeal(newMealFromChild: Meal) {
+    this.masterMealList.push(newMealFromChild);
+  }
 }
